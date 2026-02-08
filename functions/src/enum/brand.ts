127a1e947f1ad70f824319d778312977aacd3107
BottyIvan <testbottyivan@gmail.com>
2026-02-08 14:41:31 +0100

    feat: add Brand enum for game categories

---
+++ functions/src/enum/brand.ts
@@ -0,0 +1,11 @@
+export enum Brand {
+  OnePiece = "one-piece",
+  Pokemon = "pokemon",
+  DragonBallFusion = "dragon-ball-fusion",
+  Digimon = "digimon",
+  Magic = "magic",
+  UnionArena = "union-arena",
+  Gundam = "gundam",
+  Riftbound = "riftbound",
+  StarWarsUnlimited = "star-wars-unlimited",
+}

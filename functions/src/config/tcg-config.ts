1f97735cc4c840b9c2918e800a6d5731fc1675bc
BottyIvan <testbottyivan@gmail.com>
2026-02-19 22:20:36 +0100

    feat: update base URLs in TCG_CONFIG to use refs/heads/main for consistency

--- functions/src/config/tcg-config.ts
+++ functions/src/config/tcg-config.ts
@@ -22,58 +22,64 @@ import { TcgConfig } from "../interface/tcg-config";
 export const TCG_CONFIG: Record<Brand, TcgConfig> = {
   [Brand.Pokemon]: {
     repo: "pokemon-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/pokemon-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/pokemon-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.Digimon]: {
     repo: "digimon-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/digimon-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/digimon-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.OnePiece]: {
     repo: "one-piece-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/one-piece-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/one-piece-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.DragonBallFusion]: {
     repo: "dragon-ball-fusion-tcg-data",
     baseUrl:
-      "https://raw.githubusercontent.com/apitcg/dragon-ball-fusion-tcg-data/main",
+      "https://raw.githubusercontent.com/apitcg/dragon-ball-fusion-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.Magic]: {
     repo: "magic-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/magic-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/magic-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.UnionArena]: {
     repo: "union-arena-tcg-data",
     baseUrl:
-      "https://raw.githubusercontent.com/apitcg/union-arena-tcg-data/main",
+      "https://raw.githubusercontent.com/apitcg/union-arena-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.Gundam]: {
     repo: "gundam-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/gundam-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/gundam-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.Riftbound]: {
     repo: "riftbound-tcg-data",
-    baseUrl: "https://raw.githubusercontent.com/apitcg/riftbound-tcg-data/main",
+    baseUrl:
+      "https://raw.githubusercontent.com/apitcg/riftbound-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
   [Brand.StarWarsUnlimited]: {
     repo: "star-wars-unlimited-tcg-data",
     baseUrl:
-      "https://raw.githubusercontent.com/apitcg/star-wars-unlimited-tcg-data/main",
+      "https://raw.githubusercontent.com/apitcg/star-wars-unlimited-tcg-data/refs/heads/main",
     setsPath: "sets/en.json",
     cardsPath: (setId) => `cards/en/${setId}.json`,
   },
